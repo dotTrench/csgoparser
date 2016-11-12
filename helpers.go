7e380aab5ad43df45c3ff88ecb21f43d6a93950d
Erik Strandgård <erik.strandgard@gmail.com>
2016-11-12 16:55:38 +0100

    moved ParsePlayer to helpers

--- helpers.go
+++ helpers.go
@@ -1,6 +1,8 @@
 package csgoparser
 
 import (
+	"errors"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -28,6 +30,30 @@ func ParsePosition(input string) (Position, error) {
 		x: x,
 		y: y,
 		z: z,
+	}
+	return p, nil
+}
+
+var playerRegex = regexp.MustCompile(`^(.+)<(\d+)><(.+)><(.*)>$`)
+
+const ErrNoMatch = "No match"
+
+func ParsePlayer(input string) (Player, error) {
+	var p Player
+	matches := playerRegex.FindStringSubmatch(input)
+	if matches == nil {
+		return p, errors.New(ErrNoMatch)
+	}
+
+	uid, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return p, err
+	}
+	p = Player{
+		Name:    matches[1],
+		UID:     uid,
+		SteamID: matches[3],
+		Team:    matches[4],
 	}
 	return p, nil
 }
